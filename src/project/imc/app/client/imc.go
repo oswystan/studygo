67892977b4efd6906151996073b42948d358a7ef
oswystan <oswystan@126.com>
2015-12-06 19:46:51 +0800

    add new ImcProxy for client and others

--- src/project/imc/app/client/imc.go
+++ src/project/imc/app/client/imc.go
@@ -15,59 +15,7 @@ import (
 	"log"
 	"net"
 	imc "project/imc/imc_lib"
-
-	"github.com/golang/protobuf/proto"
 )
-
-func doLogin(writer *bufio.Writer, name, pwd string) error {
-	cmd := imc.CMD_TYPE_LOGIN
-	login := &imc.ImcCmd{
-		CmdType: &cmd,
-		Login: &imc.CmdLogin{
-			UserName: proto.String(name),
-			Passwd:   proto.String(pwd),
-		},
-	}
-
-	data, err := proto.Marshal(login)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	writer.Write(data)
-	writer.Flush()
-	return nil
-}
-
-func waitForResponse(reader *bufio.Reader) *imc.ImcCmd {
-	data := make([]byte, 1024)
-	reader.Read(data)
-	cmd := &imc.ImcCmd{}
-	proto.Unmarshal(data, cmd)
-	return cmd
-}
-
-func doModifyInfo(writer *bufio.Writer, name, pwd, nick string) error {
-	cmd := imc.CMD_TYPE_MODIFYINFO
-	modify := &imc.ImcCmd{
-		CmdType: &cmd,
-		ModifyInfo: &imc.CmdModifyInfo{
-			UserName:  proto.String(name),
-			NewPasswd: proto.String(pwd),
-			NickName:  proto.String(nick),
-		},
-	}
-
-	data, err := proto.Marshal(modify)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	writer.Write(data)
-	writer.Flush()
-
-	return nil
-}
 
 func main() {
 	log.Println("client started ...")
@@ -79,17 +27,10 @@ func main() {
 
 	writer := bufio.NewWriter(conn)
 	reader := bufio.NewReader(conn)
-	doLogin(writer, "wangyu", "12")
-	ack := waitForResponse(reader)
-	if *ack.AckCommon.Status != imc.RET_CODE_SUCCESS {
-		log.Println("fail to do login", *ack.AckCommon.ErrorDesc)
-	}
+	proxy := imc.NewImcProxy(reader, writer)
 
-	doModifyInfo(writer, "wangyu", "111", "wystan")
-	ack = waitForResponse(reader)
-	if *ack.AckCommon.Status != imc.RET_CODE_SUCCESS {
-		log.Println("fail to do modifyinfo", *ack.AckCommon.ErrorDesc)
-	}
+	proxy.DoLogin("wangyu", "1234")
+	proxy.DoModifyInfo("wangyu", "nopasswd", "oswystan")
 }
 
 //==================================== END ======================================
---
+++ src/project/imc/imc_lib/proxy.go
@@ -0,0 +1,93 @@
+//===============================================================================
+//                      Copyright (C) 2015 wystan
+//
+//        filename: proxy.go
+//     description:
+//         created: 2015-12-06 19:28:22
+//          author: wystan
+//
+//===============================================================================
+
+package imc_lib
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+)
+
+type ImcProxy struct {
+	writer *bufio.Writer
+	reader *bufio.Reader
+}
+
+func (p *ImcProxy) DoLogin(name, pwd string) error {
+	cmd := CMD_TYPE_LOGIN
+	login := &ImcCmd{
+		CmdType: &cmd,
+		Login: &CmdLogin{
+			UserName: proto.String(name),
+			Passwd:   proto.String(pwd),
+		},
+	}
+
+	data, err := proto.Marshal(login)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	p.writer.Write(data)
+	p.writer.Flush()
+
+	ack := p.waitForResponse(p.reader)
+	if *ack.AckCommon.Status != RET_CODE_SUCCESS {
+		return fmt.Errorf("%s", *ack.AckCommon.ErrorDesc)
+	}
+	return nil
+}
+
+func (p *ImcProxy) DoModifyInfo(name, pwd, nick string) error {
+	cmd := CMD_TYPE_MODIFYINFO
+	modify := &ImcCmd{
+		CmdType: &cmd,
+		ModifyInfo: &CmdModifyInfo{
+			UserName:  proto.String(name),
+			NewPasswd: proto.String(pwd),
+			NickName:  proto.String(nick),
+		},
+	}
+
+	data, err := proto.Marshal(modify)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	p.writer.Write(data)
+	p.writer.Flush()
+
+	ack := p.waitForResponse(p.reader)
+	if *ack.AckCommon.Status != RET_CODE_SUCCESS {
+		return fmt.Errorf("%s", *ack.AckCommon.ErrorDesc)
+	}
+	return nil
+}
+func (p *ImcProxy) waitForResponse(reader *bufio.Reader) *ImcCmd {
+	data := make([]byte, 1024)
+	p.reader.Read(data)
+	cmd := &ImcCmd{}
+	proto.Unmarshal(data, cmd)
+	return cmd
+}
+
+func NewImcProxy(r *bufio.Reader, w *bufio.Writer) *ImcProxy {
+	p := &ImcProxy{
+		reader: r,
+		writer: w,
+	}
+
+	return p
+}
+
+//==================================== END ======================================
