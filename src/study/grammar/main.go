2a5d1c9c96a3cb6ae8232bb584b517373cedb26f
oswystan <oswystan@126.com>
2015-12-09 09:46:05 +0800

    add a grammar study case

--- src/study/grammar/main.go
+++ src/study/grammar/main.go
@@ -15,6 +15,9 @@ import (
 	"time"
 )
 
+//=================================
+// 1. label usage in for loop
+//=================================
 func breakLoop() {
 	var i = 0
 Loop:
@@ -22,6 +25,7 @@ Loop:
 		switch {
 		case i > 7:
 			fmt.Printf("break %d\n", i)
+			//break the for loop
 			break Loop
 		default:
 			fmt.Printf("i=%d\n", i)
@@ -38,6 +42,7 @@ Loop:
 		switch {
 		case i > 7:
 			fmt.Printf("continue %d\n", i)
+			//continue the for loop
 			continue Loop
 		default:
 			fmt.Printf("i=%d\n", i)
@@ -47,9 +52,147 @@ Loop:
 	fmt.Printf("continue loop end i=%d\n", i)
 }
 
+//=================================
+// the internal of defer
+//=================================
+func deferUse() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Printf("i=%d\n", i)
+	}
+
+	//output:
+	//  i=4
+	//  i=3
+	//  i=2
+	//  i=1
+	//  i=0
+	// defer function is pushed into a stack.
+	// the last one is called first
+	// but the value of i is evaluated when the program
+	// run the defer statement.
+}
+
+func vardef() {
+	a := 0
+
+	// although a is defined and given a value
+	// here if there a more than one variable defined
+	// you can ALSO use := operator. so convenient.
+	v, a := 10, 20
+	fmt.Printf("v=%d,a=%d\n", v, a)
+}
+
+//=================================
+// different receiver
+//     - a value receiver will not change the value outside
+//     - a pointer receiver will change the value outside
+//=================================
+type Int int
+
+func (i Int) change() {
+	i = i + 1
+}
+func (i *Int) pchange() {
+	*i = *i + 2
+}
+
+func receiver() {
+	var i Int = 1
+	fmt.Printf("before change i = %d\n", i)
+
+	// because change() is a value receiver
+	// i will not change outside
+	i.change()
+	fmt.Printf("after change i = %d\n", i)
+
+	// pointer receiver
+	// i changed.
+	i.pchange()
+	fmt.Printf("after pchange i = %d\n", i)
+}
+
+//=================================
+// how to know the variable types
+//=================================
+func vartypes() {
+	var s interface{}
+
+	// define a string array, size is depent on the value initialized
+	s = [...]string{0: "zero", 1: "one"}
+
+	// use %T to print the type of var
+	fmt.Printf("%T\n", s)
+
+	switch t := s.(type) {
+	case string:
+		fmt.Printf("string\n")
+	case [2]string:
+		fmt.Printf("[2]string\n")
+	default:
+		fmt.Printf("unknown type %T\n", t)
+	}
+}
+
+//=================================
+// embeded type study
+//=================================
+type TypeA struct {
+}
+
+func (a *TypeA) Run() {
+	fmt.Printf("TypeA is run\n")
+}
+
+type TypeB struct {
+}
+
+func (b *TypeB) RunIt() {
+	fmt.Printf("TypeB is run\n")
+}
+
+type TypeC struct {
+	/* data */
+	*TypeA
+	*TypeB
+}
+
+func embededFunc() {
+	c := &TypeC{}
+	c.Run()
+	c.RunIt()
+}
+
+//=================================
+// visiability of member and method
+//=================================
+
+// onetype can only be used in the current package
+// and its members and methods can only be used in
+// the current package too.
+type onetype struct {
+	value int
+}
+
+func (t *onetype) change() {
+	t.value = 0
+}
+
+func visiability() {
+	t := &onetype{}
+	t.value = 1
+	t.change()
+	fmt.Printf("t.value=%d\n", t.value)
+}
+
 func main() {
-	breakLoop()
-	continueLoop()
+	//breakLoop()
+	//continueLoop()
+	//deferUse()
+	//vardef()
+	//vartypes()
+	//receiver()
+	//embededFunc()
+	//visiability()
 }
 
 //==================================== END ======================================
--- src/study/study.md
+++ src/study/study.md
@@ -1,4 +1,8 @@
 #golang建议
 - 非常好的参考文档 [Effective go](https://golang.org/doc/effective_go.html)
 - 注意成对原则，打开的文件、socket等都要用defer关闭
+
+
+#语法注意事项
+- defer func()表达式的值先被计算，然后再将func()压栈，在外层函数返回前压栈的defer函数以后进先出的顺序被调用。
 - 
