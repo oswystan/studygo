c14c09057ac5954ce78d955e8799d56accd31d9e
oswystan <oswystan@126.com>
2015-12-12 23:04:50 +0800

    implement a lazy/eager pool

---
+++ src/study/pool/resource_pool.go
@@ -0,0 +1,95 @@
+//===============================================================================
+//                      Copyright (C) 2015 wystan
+//
+//        filename: resource_pool.go
+//     description:
+//         created: 2015-12-12 22:32:03
+//          author: wystan
+//
+//===============================================================================
+
+package main
+
+import (
+	"container/list"
+	"log"
+	"sync"
+)
+
+const (
+	LAZY_POOL = iota
+	EAGER_POOL
+)
+
+type Creator func() interface{}
+
+type ResourcePool struct {
+	sync.RWMutex            //protect the following resources
+	creator      Creator    //function for creating resource
+	avaliable    *list.List //the resources that can be acquired
+	amount       int        //total amount that created
+	max          int        //how may resources we have
+}
+
+func (lp *ResourcePool) Acquire() interface{} {
+	lp.Lock()
+	defer lp.Unlock()
+	if lp.avaliable.Len() > 0 {
+		e := lp.avaliable.Front()
+		lp.avaliable.Remove(e)
+		return e.Value
+	}
+
+	if lp.amount < lp.max {
+		log.Printf("create a new one amount=%d", lp.amount)
+		rv := lp.creator()
+		lp.amount++
+		return rv
+	}
+
+	return nil
+}
+
+func (lp *ResourcePool) Release(r interface{}) {
+	lp.Lock()
+	if lp.avaliable.Len()+1 > lp.amount {
+		panic("some resouce is not in this pool")
+	}
+	lp.avaliable.PushBack(r)
+	lp.Unlock()
+}
+
+func (lp *ResourcePool) Reset() {
+	lp.Lock()
+	defer lp.Unlock()
+
+	for lp.avaliable.Front() != nil {
+		e := lp.avaliable.Front()
+		lp.avaliable.Remove(e)
+	}
+	lp.avaliable = nil
+	lp.creator = nil
+	lp.amount = 0
+	lp.max = 0
+}
+
+func NewResourcePool(poolType int, max int, fn Creator) *ResourcePool {
+	p := &ResourcePool{
+		creator:   fn,
+		avaliable: list.New(),
+		max:       max,
+		amount:    0,
+	}
+
+	if poolType == EAGER_POOL {
+		for i := 0; i < max; i++ {
+			v := p.creator()
+			p.avaliable.PushBack(v)
+		}
+		p.amount = max
+	}
+
+	return p
+}
+
+//==================================== END ======================================
---
+++ src/study/pool/resource_pool_test.go
@@ -0,0 +1,81 @@
+//===============================================================================
+//                      Copyright (C) 2015 wystan
+//
+//        filename: resource_pool_test.go
+//     description:
+//         created: 2015-12-12 22:40:11
+//          author: wystan
+//
+//===============================================================================
+package main
+
+import "testing"
+
+type item struct {
+	/* data */
+	value int
+}
+
+func newItem() interface{} {
+	return &item{value: 1}
+}
+
+func TestLazyPool(t *testing.T) {
+	p := NewResourcePool(LAZY_POOL, 10, newItem)
+	var slice []*item = make([]*item, 10)
+	for i := 0; i < 10; i++ {
+		v := p.Acquire()
+		if v == nil {
+			t.Errorf("fail to acquire resource")
+		}
+		slice[i] = v.(*item)
+		if slice[i].value != 1 {
+			t.Errorf("invalid item value=%d", slice[i].value)
+		}
+	}
+
+	v := p.Acquire()
+	if v != nil {
+		t.Errorf("acquire too much resource but return one")
+	}
+
+	for i := 0; i < 10; i++ {
+		p.Release(slice[i])
+	}
+
+	for i := 0; i < 10; i++ {
+		v := p.Acquire()
+		if v == nil {
+			t.Errorf("fail to acquire resource")
+		}
+		slice[i] = v.(*item)
+		if slice[i].value != 1 {
+			t.Errorf("invalid item value=%d", slice[i].value)
+		}
+	}
+
+	p.Reset()
+}
+
+func TestEagerPool(t *testing.T) {
+	p := NewResourcePool(EAGER_POOL, 10, newItem)
+	var slice []*item = make([]*item, 10)
+	for i := 0; i < 10; i++ {
+		v := p.Acquire()
+		if v == nil {
+			t.Errorf("fail to acquire resource")
+		}
+		slice[i] = v.(*item)
+	}
+
+	v := p.Acquire()
+	if v != nil {
+		t.Errorf("acquire too much resource but return one")
+	}
+
+	for i := 0; i < 10; i++ {
+		p.Release(slice[i])
+	}
+}
+
+//==================================== END ======================================
