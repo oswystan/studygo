b956653416e9818aae97aa7ca3932e120c830d1e
oswystan <oswystan@126.com>
2016-02-16 10:52:50 +0800

    add gc.go

---
+++ src/study/grammar/gc.go
@@ -0,0 +1,17 @@
+//===============================================================================
+//                      Copyright (C) 2016 wystan
+//
+//        filename: gc.go
+//     description:
+//         created: 2016-02-16 10:48:32
+//          author: wystan
+//
+//===============================================================================
+
+package main
+
+// the following files are golang gc implentations
+// $GOSRC/src/runtime:
+//		mgc.go mgcmark.go mgcsweep.go mgcwork.go
+
+//==================================== END ======================================
--- src/study/tool/main.go
+++ src/study/tool/main.go
@@ -10,7 +10,7 @@
 
 // there are 3 init files here: run_linux.go run_windows.go run_darwin.go
 // but only the init() in run_darwin.go will be linked into the final elf file
-// that is because `go too dist` will ignore other os specific files;
+// that is because `go tool dist` will ignore other os specific files;
 // please refer to the file $GO_SRC/cmd/dist/build.go
 
 package main
